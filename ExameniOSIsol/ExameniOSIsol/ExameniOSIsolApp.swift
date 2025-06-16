b9ba289d0a8f762b5e92d97eb0ea0458df4cc2a5
José Antonio Campos <fred-mercury@hotmail.com>
2025-06-16 16:34:18 -0600

    Firts commit

---
+++ ExameniOSIsol/ExameniOSIsol/ExameniOSIsolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExameniOSIsolApp.swift
+//  ExameniOSIsol
+//
+//  Created by Antonio Bedolla on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ExameniOSIsolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
